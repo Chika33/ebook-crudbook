125f02217ce76a8f1779ecf107510e3e8e27ca8b
Chika Nur Alifa <3103119047@student.smktelkom-pwt.sch.id>
2021-12-19 15:16:54 +0700

    sanctum

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,19 +3,67 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
+use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
-    public function me(){
-        return
-        [
+    public function me()
+    {
+        return[
             "NISN" => 3103119037,
             "Nama" => "Bagas Makhali",
             "Gender" => "Laki-laki",
             "Phone" => 6282313666691,
             "Kelas" => "XII RPL 2"
         ];
-
     }
 
+        // R E G I S T E R
+  public function register(Request $request) {
+    $fields = $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|string|unique:users,email',
+        'password' => 'required|string|confirmed'
+    ]);
+    $user = User::create([
+        'name' => $fields['name'],
+        'email' => $fields['email'],
+        'password' => bcrypt($fields['password'])
+    ]);
+    $token = $user->createToken('myapptoken')->plainTextToken;
+    $response = [
+        'user' => $user,
+        'token' => $token
+    ];
+    return response($response, 201);
+  }
+
+  // L O G I N
+  public function login(Request $request) {
+      $fields = $request->validate([
+          'email' => 'required|string',
+          'password' => 'required|string'
+      ]);
+      $user = User::where('email', $fields['email'])->first();
+      if(!$user || !Hash::check($fields['password'], $user->password)) {
+          return response([
+              'message' => 'Bad creds'
+          ], 401);
+      }
+      $token = $user->createToken('myapptoken')->plainTextToken;
+      $response = [
+          'user' => $user,
+          'token' => $token
+      ];
+      return response($response, 201);
+  }
+
+  // L O G O U T
+  public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return [
+            'message' => 'Logged out'
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,13 +17,28 @@ use App\Http\Controllers\BookController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::get('/book', [BookController::class, 'index']);
-Route::post('/book', [BookController::class, 'store']);
-Route::get('/book/{id}', [BookController::class, 'show']);
-Route::put('/book/{id}', [BookController::class, 'update']);
-Route::delete('/book/{id}', [BookController::class, 'destroy']);
+// Route::get('/book', [BookController::class, 'index']);
+// Route::post('/book', [BookController::class, 'store']);
+// Route::get('/book/{id}', [BookController::class, 'show']);
+// Route::put('/book/{id}', [BookController::class, 'update']);
+// Route::delete('/book/{id}', [BookController::class, 'destroy']);
 Route::resource('book', BookController::class)->except('edit', 'create');
+Route::resource('author', AuthorController::class)->except('edit', 'create');
+
+//  login register
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// protected
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('book', BookController::class)->except('edit', 'create');
+    Route::resource('author', AuthorController::class)->except('edit', 'create');
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
